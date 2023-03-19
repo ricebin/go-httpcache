afb164d685fb14c82f097af482520956593d34e3
ricebin <ricebin@users.noreply.github.com>
2023-03-19 12:08:43 -0700

    write version and expiration

--- httpcache/redis/cache.go
+++ httpcache/redis/cache.go
@@ -2,9 +2,15 @@ package redis
 
 import (
 	"context"
+	"encoding/binary"
+	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
+)
+
+const (
+	version = 1
 )
 
 type Result struct {
@@ -16,25 +22,57 @@ func (r *Result) Value() []byte {
 }
 
 type Cache struct {
-	c *redis.Client
+	c   *redis.Client
+	now func() time.Time
 }
 
 func New(c *redis.Client) *Cache {
-	return &Cache{c: c}
+	return NewWithClock(c, time.Now)
 }
 
-func (c *Cache) Get(ctx context.Context, url string) ([]byte, error) {
+func NewWithClock(c *redis.Client, now func() time.Time) *Cache {
+	return &Cache{c: c, now: now}
+}
+
+func (c *Cache) Get(ctx context.Context, url string) ([]byte, *time.Time, error) {
 	b, err := c.c.Get(ctx, url).Bytes()
 	if err == redis.Nil {
-		return nil, nil
+		return nil, nil, nil
 	} else if err != nil {
-		return nil, err
+		return nil, nil, err
 	} else {
-		return b, nil
+		cachedVersion, bytesRead := binary.Uvarint(b)
+		if bytesRead == 0 {
+			return nil, nil, fmt.Errorf("unexpected format. unable to read version")
+		}
+		if version != cachedVersion {
+			return nil, nil, nil
+		}
+
+		b = b[bytesRead:]
+		insertionUnixSecs, bytesRead := binary.Uvarint(b)
+
+		b = b[bytesRead:]
+		insertion := time.Unix(int64(insertionUnixSecs), 0)
+		return b, &insertion, nil
 	}
 }
 
-func (c *Cache) Set(ctx context.Context, url string, body []byte, expiration time.Duration) error {
-	_, err := c.c.Set(ctx, url, body, expiration).Result()
-	return err
+func (c *Cache) Set(ctx context.Context, url string, rawResponse []byte, expiration time.Duration) error {
+	// uvarint: version
+	// uvarint: insertion time in unix sec
+	// payload
+	v := make([]byte, 0, binary.MaxVarintLen64+binary.MaxVarintLen64+len(rawResponse))
+	v = binary.AppendUvarint(v, version)
+	v = binary.AppendUvarint(v, uint64(c.now().Unix()))
+	v = append(v, rawResponse...)
+
+	result, err := c.c.Set(ctx, url, v, expiration).Result()
+	if err != nil {
+		return err
+	} else if result != "OK" {
+		return fmt.Errorf("redis.Set failed: %s", result)
+	} else {
+		return nil
+	}
 }
--- httpcache/redis/cache_test.go
+++ httpcache/redis/cache_test.go
@@ -1,6 +1,7 @@
 package redis_test
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
@@ -25,6 +26,10 @@ func TestWrap_Happy(t *testing.T) {
 	}))
 	defer ts.Close()
 
+	clock := &fakeClock{
+		now: time.Now(),
+	}
+
 	s := miniredis.RunT(t)
 	defer s.Close()
 	rc := redis.NewClient(&redis.Options{
@@ -32,33 +37,78 @@ func TestWrap_Happy(t *testing.T) {
 	})
 	defer rc.Close()
 
-	cache := httpcache_redis.New(rc)
-	cacheTransport := roundtripper.Wrap(http.DefaultTransport, cache)
+	cache := httpcache_redis.NewWithClock(rc, clock.Now)
+	cacheTransport := roundtripper.WrapWithClock(http.DefaultTransport, cache, clock.Now)
 
 	hc := &http.Client{
 		Transport: cacheTransport,
 	}
 
 	expiration := 1 * time.Hour
-	assertResponseBody(t, hc, ts.URL+"/one", &expiration, "GET:0:/one")
+	assertResponseBody(t, hc, ts.URL+"/one", &expiration, []byte("GET:0:/one"))
 
 	// increment clock
 	s.FastForward(30 * time.Minute)
+	clock.Add(30 * time.Minute)
 
 	// read the same url, assert cache hit
-	assertResponseBody(t, hc, ts.URL+"/one", &expiration, "GET:0:/one")
-	assertResponseBody(t, hc, ts.URL+"/two", &expiration, "GET:0:/two")
+	assertResponseBody(t, hc, ts.URL+"/one", &expiration, []byte("GET:0:/one"))
+	assertResponseBody(t, hc, ts.URL+"/two", &expiration, []byte("GET:0:/two"))
 
 	// increment clock
 	s.FastForward(45 * time.Minute)
+	clock.Add(45 * time.Minute)
 
 	// one expired
-	assertResponseBody(t, hc, ts.URL+"/one", &expiration, "GET:1:/one")
+	assertResponseBody(t, hc, ts.URL+"/one", &expiration, []byte("GET:1:/one"))
 	// two hasnt expired
-	assertResponseBody(t, hc, ts.URL+"/two", &expiration, "GET:0:/two")
+	assertResponseBody(t, hc, ts.URL+"/two", &expiration, []byte("GET:0:/two"))
+
+	// query two with different expiration
+	{
+		newExpiration := 1 * time.Minute
+		assertResponseBody(t, hc, ts.URL+"/two", &newExpiration, []byte("GET:1:/two"))
+	}
 }
 
-func assertResponseBody(t *testing.T, hc *http.Client, url string, expiration *time.Duration, expected string) {
+func TestWrap_Binary(t *testing.T) {
+	payload := []byte{1, 2, 3, 4, 5}
+	counter := 0
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		counter = counter + 1
+		w.Write(payload)
+	}))
+	defer ts.Close()
+
+	clock := &fakeClock{
+		now: time.Now(),
+	}
+
+	s := miniredis.RunT(t)
+	defer s.Close()
+	rc := redis.NewClient(&redis.Options{
+		Addr: s.Addr(),
+	})
+	defer rc.Close()
+
+	cache := httpcache_redis.NewWithClock(rc, clock.Now)
+	cacheTransport := roundtripper.WrapWithClock(http.DefaultTransport, cache, clock.Now)
+
+	hc := &http.Client{
+		Transport: cacheTransport,
+	}
+
+	oneHourExpiration := 1 * time.Hour
+	assertResponseBody(t, hc, ts.URL+"/one", &oneHourExpiration, payload)
+
+	clock.Add(30 * time.Minute)
+	assertResponseBody(t, hc, ts.URL+"/one", &oneHourExpiration, payload)
+	if counter != 1 {
+		t.Errorf("expected counter to be 1")
+	}
+}
+
+func assertResponseBody(t *testing.T, hc *http.Client, url string, expiration *time.Duration, expected []byte) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		t.Fatal(err)
@@ -73,8 +123,20 @@ func assertResponseBody(t *testing.T, hc *http.Client, url string, expiration *t
 		if err != nil {
 			t.Fatal(err)
 		}
-		if string(got) != expected {
+		if !bytes.Equal(got, expected) {
 			t.Errorf("expectect: %v but got: %v", expected, string(got))
 		}
 	}
+}
+
+type fakeClock struct {
+	now time.Time
+}
+
+func (c *fakeClock) Now() time.Time {
+	return c.now
+}
+
+func (c *fakeClock) Add(d time.Duration) {
+	c.now = c.now.Add(d)
 }
